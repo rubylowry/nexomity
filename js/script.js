b17f0fb4a1052e13b773415f97be214d5cd2054b
Ruby Lowry <270010549@bun2-205-19.acg.ac.nz>
2019-09-13 21:57:57 +1200

    made hamburger navigation menu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// This is how you can click on my hamburger menu
+
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change");
+
+    document.getElementById("nav").classList.toggle("change");
+
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}

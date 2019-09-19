def5023a319fbc3fb173f6fddf918d19a5fa437c
Ruby Lowry <rubylowry@outlook.com>
2019-09-20 00:36:55 +1200

    mobile lights page complete

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function onClickMenu(){
 // This is my slider on lights page
 
 
-// /*This affects the slider*/
+/*This affects the slider*/
 var slider = document.getElementById("myRange");
 /*This affects the value change*/
 var output = document.getElementById("value");
@@ -26,38 +26,4 @@ slider.addEventListener ("mousemove", function(){
   var x = slider.value;
   var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
   slider.style.background = color;
-})
-
-
-const cameraSlider = document.querySelector('.camera__slider');
-const cameraProgress = document.querySelector('.camera__slider .camera__progress');
-const cameraThumb = document.querySelector('.camera__slider .camera__thumb');
-const cameraSliderWidth = cameraSlider.offsetWidth;
-const cameraSliderLeft = cameraSlider.offsetLeft;
-
-var percentage = 50;
-var dragging = false;
-var translate;
-
-
-
-function setPercentage() {
-cameraProgress.style.transform = 'scaleX(' + percentage/100 + ')';
-cameraThumb.style.transform = 'translateX(' + percentage/100 * cameraSliderWidth) + 'px)';
-}
-setPercentage();
-
-thumb.addEventListener('mousedown', function(e) {
-  dragging = true;
-});
-document.addEventListener('mousemove', function(e) {
-  if (dragging) {
-    translate = e.clientX - cameraSliderLeft;
-    percentage = (translate /cameraSliderWidth) * 100;
-    setPercentage();
-    cameraProgress.style.transform = 'scaleX(' + + ')';
-  }
-})
-document.addEventListener('mouseup', function(e) {
-  dragging = false;
 })

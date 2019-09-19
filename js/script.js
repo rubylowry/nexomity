8be0f4d67cce2122c5c9f9f5e2cb40274665b383
Ruby Lowry <270010549@bun2-206-15.local>
2019-09-19 15:50:40 +1200

    got slider range working

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function onClickMenu(){
 // This is my slider on lights page
 
 
-/*This affects the slider*/
+// /*This affects the slider*/
 var slider = document.getElementById("myRange");
 /*This affects the value heading*/
 var output = document.getElementById("value");
@@ -24,6 +24,13 @@ slider.oninput = function() {
 
 slider.addEventListener("mousemove", function(){
   var x = slider.value;
-  var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%);
+  var color1 = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
   slider.style.background = color;
 })
+
+// var ARRAY_INDEX = 0; // You better declare this var outside the event handler function if you want to use it else where
+
+// $("#slider").on("change",function(){
+//     ARRAY_INDEX = $(this).val();
+//     console.log(ARRAY_INDEX);
+// });

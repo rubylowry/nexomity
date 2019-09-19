c96489893b718ba33e209a0cf45378a3a86a9c9e
Ruby Lowry <270010549@bun2-206-15.local>
2019-09-19 15:18:08 +1200

    working on slider

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ function onClickMenu(){
 
     document.getElementById("menu__bg").classList.toggle("change__bg");
 }
+
+// This is my slider on lights page
+
+
+/*This affects the slider*/
+var slider = document.getElementById("myRange");
+/*This affects the value heading*/
+var output = document.getElementById("value");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+slider.addEventListener("mousemove", function(){
+  var x = slider.value;
+  var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%);
+  slider.style.background = color;
+})

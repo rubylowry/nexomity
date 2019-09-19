bc492d6389e9b827e8d70bebc3312b7027179202
Ruby Lowry <270010549@bun2-205-19.acg.ac.nz>
2019-09-19 22:42:36 +1200

    javascript slider

--- js/script.js
+++ js/script.js
@@ -27,3 +27,37 @@ slider.addEventListener ("mousemove", function(){
   var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
   slider.style.background = color;
 })
+
+
+const cameraSlider = document.querySelector('.camera__slider');
+const cameraProgress = document.querySelector('.camera__slider .camera__progress');
+const cameraThumb = document.querySelector('.camera__slider .camera__thumb');
+const cameraSliderWidth = cameraSlider.offsetWidth;
+const cameraSliderLeft = cameraSlider.offsetLeft;
+
+var percentage = 50;
+var dragging = false;
+var translate;
+
+
+
+function setPercentage() {
+cameraProgress.style.transform = 'scaleX(' + percentage/100 + ')';
+cameraThumb.style.transform = 'translateX(' + percentage/100 * cameraSliderWidth) + 'px)';
+}
+setPercentage();
+
+thumb.addEventListener('mousedown', function(e) {
+  dragging = true;
+});
+document.addEventListener('mousemove', function(e) {
+  if (dragging) {
+    translate = e.clientX - cameraSliderLeft;
+    percentage = (translate /cameraSliderWidth) * 100;
+    setPercentage();
+    cameraProgress.style.transform = 'scaleX(' + + ')';
+  }
+})
+document.addEventListener('mouseup', function(e) {
+  dragging = false;
+})

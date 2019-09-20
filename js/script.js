ef5e09d8c9ae33faa14700bd03d87e5f703be96a
Ruby Lowry <rubylowry@outlook.com>
2019-09-21 01:00:13 +1200

    working on ac page

--- js/script.js
+++ js/script.js
@@ -7,27 +7,3 @@ function onClickMenu(){
 
     document.getElementById("menu__bg").classList.toggle("change__bg");
 }
-
-// This is my slider on lights page
-
-
-/*This affects the slider*/
-var slider = document.getElementById("myRange");
-/*This affects the value change*/
-var output = document.getElementById("value");
-
-var outputhr = document.getElementById("valuehr");
-
-output.innerHTML = slider.value;
-outputhr.innerHTML = slider.value/10;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-  outputhr.innerHTML = this.value/10;
-}
-
-slider.addEventListener ("mousemove", function(){
-  var x = slider.value;
-  var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
-  slider.style.background = color;
-})
---
+++ js/slider.js
@@ -0,0 +1,21 @@
+// This is my slider on lights page
+
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+var outputhr = document.getElementById("valuehr");
+
+output.innerHTML = slider.value;
+outputhr.innerHTML = slider.value/10;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  outputhr.innerHTML = this.value/10;
+}
+
+slider.addEventListener ("mousemove", function(){
+  var x = slider.value;
+  var color = 'linear-gradient(90deg, rgb(255,251,0)' + x + '%, rgb(214,214,214)' + x + '%)';
+  slider.style.background = color;
+})
